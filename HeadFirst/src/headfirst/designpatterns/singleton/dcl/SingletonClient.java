cd58803752c50419a5e1bdb541e5897d11cfa3bd
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2022-06-18 17:43:16 +0800

    add dcl

---
+++ HeadFirst/src/headfirst/designpatterns/singleton/dcl/SingletonClient.java
@@ -0,0 +1,8 @@
+package headfirst.designpatterns.singleton.dcl;
+
+public class SingletonClient {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+    }
+}
+
